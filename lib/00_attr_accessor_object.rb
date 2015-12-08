1be204cff248f96b8f26af6e754b9ae454fe12ff
Jamin <jamincheung1@gmail.com>
2015-12-07 23:04:39 -0500

    Create AttrAccessor methods.

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,14 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    variables = names
+    variables.each do |variable|
+      define_method(variable) do
+        self.instance_variable_get("@#{variable}".to_sym)
+      end
+
+      define_method("#{variable}=") do |value|
+        instance_variable_set("@#{variable}", value)
+      end
+    end
   end
 end
