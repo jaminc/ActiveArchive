41ff0df931df9964fd3a984f9bbb623c73dfd6b5
Jamin <jamincheung1@gmail.com>
2015-12-07 23:40:02 -0500

    Add finalize setter and getter methods for table columns.

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -3,10 +3,29 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    return @columns if @columns
+    cols = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+        0
+    SQL
+
+    @columns = cols.map(&:to_sym)
   end
 
   def self.finalize!
+    self.columns.each do |name|
+      define_method(name) do
+        self.attributes[name]
+      end
+
+      define_method("#{name}=") do |value|
+        self.attributes[name] = value
+      end
+    end
   end
 
   def self.table_name=(table_name)
@@ -34,7 +53,7 @@ class SQLObject
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
