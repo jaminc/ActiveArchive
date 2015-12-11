8dfd6c7aacda629118baf8df3e934b7d465faf9e
Jamin <jamincheung1@gmail.com>
2015-12-11 15:03:31 -0500

    Reorganize files.

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -1,52 +1,5 @@
-require_relative 'searchable'
-require 'active_support/inflector'
-
-class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
-
-  def model_class
-    @class_name.constantize
-  end
-
-  def table_name
-    model_class.table_name
-  end
-end
-
-class BelongsToOptions < AssocOptions
-  def initialize(name, options = {})
-    defaults = {
-      class_name: name.to_s.camelcase,
-      foreign_key: "#{name}_id".to_sym,
-      primary_key: :id
-    }
-
-    defaults.keys.each do |key|
-      self.send("#{key}=", options[key] || defaults[key])
-    end
-  end
-end
-
-class HasManyOptions < AssocOptions
-  def initialize(name, self_class_name, options = {})
-    defaults = {
-      class_name: name.to_s.singularize.camelcase,
-      foreign_key: "#{self_class_name.underscore}_id".to_sym,
-      primary_key: :id
-    }
-
-    defaults.keys.each do |key|
-      self.send("#{key}=", options[key] || defaults[key])
-    end
-  end
-end
-
 module Associatable
-  # Phase IIIb
+
   def belongs_to(name, options = {})
     self.assoc_options[name] = BelongsToOptions.new(name, options)
 
@@ -77,8 +30,36 @@ module Associatable
   def assoc_options
     @assoc_options ||= {}
   end
-end
 
-class SQLObject
-  extend Associatable
+  def has_one_through(name, through_name, source_name)
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      through_primary_key = through_options.primary_key
+      through_foreign_key = through_options.foreign_key
+
+      source_table = source_options.table_name
+      source_primary_key = source_options.primary_key
+      source_foreign_key =source_options.foreign_key
+
+      key_val = self.send(through_foreign_key)
+      results = DBConnection.execute(<<-SQL, key_val)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+        ON
+          #{through_table}.#{source_foreign_key} = #{source_table}.#{source_primary_key}
+        WHERE
+          #{through_table}.#{through_primary_key} = ?
+      SQL
+
+      source_options.model_class.parse_all(results).first
+    end
+  end
+
 end
---
+++ lib/associations.rb
@@ -0,0 +1,51 @@
+require_relative 'searchable'
+require 'associatable'
+require 'active_support/inflector'
+
+class AssocOptions
+  attr_accessor(
+    :foreign_key,
+    :class_name,
+    :primary_key
+  )
+
+  def model_class
+    @class_name.constantize
+  end
+
+  def table_name
+    model_class.table_name
+  end
+end
+
+class BelongsToOptions < AssocOptions
+  def initialize(name, options = {})
+    defaults = {
+      class_name: name.to_s.camelcase,
+      foreign_key: "#{name}_id".to_sym,
+      primary_key: :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
+  end
+end
+
+class HasManyOptions < AssocOptions
+  def initialize(name, self_class_name, options = {})
+    defaults = {
+      class_name: name.to_s.singularize.camelcase,
+      foreign_key: "#{self_class_name.underscore}_id".to_sym,
+      primary_key: :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
+  end
+end
+
+class SQLObject
+  extend Associatable
+end
